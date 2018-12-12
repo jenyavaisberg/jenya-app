6ca77edcb851c1f35a18574ab4281733a359ee68
jenyavaisberg <v.jenya@gmail.com>
2018-12-12 19:52:31 +0200

    my project

---
+++ scripts/myapp.js
@@ -0,0 +1,40 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+  .when ('/homepage', {
+    templateURL:'views/homepage.html'
+    })
+
+  .when ('/rooms', {
+      templateURL:'views/rooms.html',
+      controller: 'RoomsController'
+    })
+
+  .otherwise ({redirectTo:'/homepage'});
+
+}]);
+
+myApp.controller('RoomsController',['$scope','$http', function ($scope,$http){
+  $scope.removeRoom =function (room) {
+    var removeRoom=$scope.rooms.indexOf(room);
+    $scope.rooms.splice(removeRoom,1);
+  }
+$scope.addRoom=function(){
+  $scope.rooms.push({
+    name: $scope.newroom.name,
+    size: $scope.newroom.size,
+    type: $scope.newroom.type,
+    available:true
+  });
+   $scope.newroom.name="";
+   $scope.newroom.size="";
+   $scope.newroom.type="";
+   available:true
+}
+;
+
+$http.get('data/rooms.json').success(function(data){
+  $scope.rooms=data;
+});
+}]);
